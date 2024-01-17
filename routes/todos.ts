d68519511ffc5657b7224f0e468797c4049ebf39
yatin goyal <yatingoyal31@gmail.com>
2024-01-17 15:25:46 +0530

    added remaining route

--- routes/todos.ts
+++ routes/todos.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 import { Todo } from '../models/todo';
 
-const todos: Todo[] = [];
+let todos: Todo[] = [];
 
 const router = express.Router();
 
@@ -16,6 +16,22 @@ router.post('/todo',(req,res,next) => {
        text: req.body.text,
     };
     todos.push(newTodo);
+    res.status(201).json({message: 'Added Todo', todo:newTodo, todos: todos});
+});
+
+router.put('/todo/:todoId',(req,res,next) => {
+  const tid = req.params.todoId;
+  const todoIndex = todos.findIndex(todoItem => todoItem.id === tid);
+  if(todoIndex >= 0){
+    todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text};
+    return res.status(200).json({messsage: 'Updated todo', todos: todos});
+  }
+  res.status(404).json({message: 'Could not find todo for this id.'});
+});
+
+router.delete('/todo/:todoId',(req,res,next) => {
+  todos = todos.filter(todoItem => todoItem.id !== req.params.todoId);
+  res.status(200).json({message: 'Deleted todo', todos: todos});
 });
 
 export default router;
